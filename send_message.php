53a9443a59f44b8ed43c4ede2179d72f5101addf
zeneralk28 <masuelaglen@gmail.com>
2024-09-30 23:13:15 +0800

    exe 5 changes = message_receipt and send_message

--- send_message.php
+++ send_message.php
@@ -5,6 +5,7 @@ $dbname = 'dbpersonal';
 $username = 'root';
 $password = ''; 
 
+/*
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -41,4 +42,48 @@ try {
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
+
+*/
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+
+  //POST request and form validation
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = test_input($_POST['name']);
+    $email = test_input($_POST['email']);
+    $recipient = test_input($_POST['recipient']);
+    $message = test_input($_POST['message']);
+
+    $stmt = $conn->prepare("INSERT INTO messages (name, email, recipient, message) VALUES (?, ?, ?, ?)");
+
+    if ($stmt === false) {
+        die("Error preparing statement: " . $conn->error);
+      }
+
+    $stmt->bind_Param("ssss", $name, $email, $recipient, $message);
+
+    if ($stmt->execute()) {
+
+        header("Location: message_receipt.php?name=$name&email=$email&recipient=$recipient&message=$message");
+        exit();
+    } else {
+        echo "Failed to send the message.";
+    }
+
+  }
+
+$stmt->close();
+$conn->close();
 ?>
